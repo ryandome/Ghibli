edcd95742429ba69313b56081e4e3c7154e40256
Connor A. Smith <c.smith@centre.edu>
2019-10-10 11:00:26 -0400

    work

--- ghibli.js
+++ ghibli.js
@@ -1,1 +1,25 @@
+var setBanner = function(message)
+{
+   d3.select("#banner")
+             .text(message)}
 
+
+var ghibliPromise = d3.json("https://ghibliapi.herokuapp.com/films")
+
+ghibliPromise.then(
+function(data)
+{
+   setBanner("select a film");
+    console.log("works",data);
+},
+function(err)
+{
+    console.log("broken",err);
+    
+}
+
+
+
+
+
+)   
