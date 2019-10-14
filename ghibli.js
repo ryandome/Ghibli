de6c550c9de02ac4f27c7dae733ed25bad33aa3a
ryandome <54848905+ryandome@users.noreply.github.com>
2019-10-14 17:50:59 -0400

    add

--- ghibli.js
+++ ghibli.js
@@ -9,17 +9,40 @@ var ghibliPromise = d3.json("https://ghibliapi.herokuapp.com/films")
 ghibliPromise.then(
 function(data)
 {
-   setBanner("select a film");
+    setBanner("Select a film");
     console.log("works",data);
 },
 function(err)
 {
     console.log("broken",err);
     
+})   
+
+
+
+var displayInfo = function(films)
+{
+  var box = d3.select("#one").append("div");
+    var items = box.selectAll("p").data(films).enter().append("p");
+    items.append("span").text(function(d){
+        return(d).title;
+        return(d).description;
+});
+
+ 
+    
 }
 
+ghibliPromise.then(
+function(data)
+{
+    displayInfo(data);
+    console.log("works",data);
+},
+function(err)
+{
+    console.log("broken",err);
+    
+})   
 
 
-
-
-)   
